f8d3ebd5a07a76743905c83b250cfece6d59286c
drmmx <m.daglarov@gmail.com>
2018-04-22 14:42:45 +0300

    xml commit

--- app/src/main/java/com/mdagl/favsports/MainActivity.java
+++ app/src/main/java/com/mdagl/favsports/MainActivity.java
@@ -1,9 +1,8 @@
 package com.mdagl.favsports;
 
 import android.content.Context;
-import android.graphics.Color;
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +10,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
             LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View actionBarView = inflater.inflate(R.layout.custom_abb_bar_layout, null);
             actionBar.setCustomView(actionBarView);
+
+            CircleImageView imageView = findViewById(R.id.profileThumbImage);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), ProfileActivity.class);
+                    startActivity(intent);
+                }
+            });
         }
 
 
